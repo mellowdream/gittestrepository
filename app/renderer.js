f4c698051051acf61d16839bf63dda9fddd58376
thinkdj <think.dj@gmail.com>
2020-07-12 22:17:05 +0530

    base: electron 9.1.0, @electron-forge

---
+++ app/renderer.js
@@ -0,0 +1,18 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+const myNotification = new Notification('Title', {
+    body: 'Lorem Ipsum Dolor Sit Amet'
+})
+
+myNotification.onclick = () => {
+    console.log('Notification clicked')
+}
+myNotification.onshow = () => {     console.log('Notification showed') }
+
+let config = require('./config.json');
+alert(config.firstName + ' ' + config.lastName);

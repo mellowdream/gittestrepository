3f29f78db7e82ea0346e592b344e643e1aba83d0
thinkdj <think.dj@gmail.com>
2020-07-14 19:30:19 +0530

    jQuery init() moved to .js

---
+++ src/app/assets/js/init.js
@@ -0,0 +1,7 @@
+/*window.nodeRequire = require;
+delete window.require;
+delete window.exports;
+delete window.module;*/
+//window.$ = window.jQuery = require( __dirname + '/assets/js/jquery-1.12.2.min.js');
+
+window.$ = window.jQuery = require('jquery');

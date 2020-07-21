8d691ff1981e127e4aea28964105cfa5cc10ad47
thinkdj <think.dj@gmail.com>
2020-07-21 09:14:03 +0530

    travis.yml updated deploy.sh LICENSE

---
+++ deploy.travis.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# SRC: https://qvault.io/2019/08/08/automatic-cross-platform-deployments-with-electron-on-a-ci-server-travis/
+
+if [ "$TRAVIS_OS_NAME" == osx ]; then
+    # deploy on mac
+    yarn release
+else
+    # deploy on windows and linux
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn release --linux AppImage --win"
+fi
